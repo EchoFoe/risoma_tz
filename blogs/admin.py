55301b3cb1fd182f6e2450e9be6a0c0a3caf2f99
EchoFoe <aspirpd@gmail.com>
2024-05-03 20:34:42 +0400

    Blogs: создание модели комментариев, связанной с юзером O-t-M и с постом O-t-M + кастомизация админ-панели

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Blog, Post
+from .models import Blog, Post, Comment
 
 
 class PostInline(admin.StackedInline):
@@ -28,9 +28,45 @@ class BlogAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'is_active']
     list_filter = ['is_active']
     list_display_links = ['id']
-    list_per_page = 30
+    list_per_page = 50
     search_fields = ['user', 'id']
     fieldsets = (
         ('Основная информация', {'fields': (('user', 'is_active'),)}),
+        ('Даты', {'fields': (('created_at', 'updated_at'),)}),
+    )
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """ Админ-панель для Blog """
+
+    save_as = True
+    readonly_fields = ['created_at', 'updated_at']
+    list_display = ['id', 'blog', 'title']
+    list_filter = ['is_active']
+    list_display_links = ['id']
+    list_per_page = 50
+    search_fields = ['blog', 'title']
+    fieldsets = (
+        ('Основная информация', {'fields': (('title', 'blog', 'is_active'),)}),
+        ('Контент', {'fields': ('content',)}),
+        ('Даты', {'fields': (('created_at', 'updated_at'),)}),
+    )
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """ Админ-панель для Comment """
+
+    save_as = True
+    readonly_fields = ['created_at', 'updated_at']
+    list_display = ['id', 'post', 'author']
+    list_filter = ['is_active']
+    list_display_links = ['id']
+    list_per_page = 50
+    search_fields = ['author', 'post__title']
+    fieldsets = (
+        ('Основная информация', {'fields': (('post', 'author', 'is_active'),)}),
+        ('Текст комментария', {'fields': ('text',)}),
         ('Даты', {'fields': (('created_at', 'updated_at'),)}),
     )
--- blogs/models.py
+++ blogs/models.py
@@ -25,4 +25,19 @@ class Post(DateTimeBaseModel):
         verbose_name_plural = 'Посты'
 
     def __str__(self):
-        return f'Пост {self.title} пользователя: {self.blog.user.username}'
+        return f'{self.title}'
+
+
+class Comment(DateTimeBaseModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', verbose_name='Пост')
+    author = models.ForeignKey('accounts.Account', on_delete=models.CASCADE, verbose_name='Автор комментария')
+    text = models.TextField(
+        max_length=200, blank=True, verbose_name='Текст комментария', help_text='Не более 200 символов'
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return f'Комментарий от {self.author.username} к посту "{self.post.title}"'
